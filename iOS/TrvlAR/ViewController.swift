c4989e75e3ccf9320c7571421e0fa3f951c8a6f2
Avery Lamp <averylamp@gmail.com>
2017-09-16 22:04:54 -0400

    New AR Hit tests for object

--- iOS/TrvlAR/ViewController.swift
+++ iOS/TrvlAR/ViewController.swift
@@ -13,11 +13,9 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var sceneView: ARSCNView!
-
-    var planeCount = 0
-    var currentPlane:SCNNode?
-	var debuggingLabel = UILabel()
-	
+    
+    var debuggingLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,22 +23,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.automaticallyUpdatesLighting = false
         
         let tap = UITapGestureRecognizer()
-		tap.numberOfTouchesRequired = 1
+        tap.numberOfTouchesRequired = 1
         tap.addTarget(self, action: #selector(didTap))
         sceneView.addGestureRecognizer(tap)
-		
-		//Three Finger Tap for Reset
-		let threeFingerTap = UITapGestureRecognizer()
-		threeFingerTap.numberOfTouchesRequired = 3
-		threeFingerTap.addTarget(self, action: #selector(didThreeFingerTap))
-		sceneView.addGestureRecognizer(threeFingerTap)
-		
-		
-		//Debugging Label
-		debuggingLabel.frame = CGRect(x: 0, y: self.view.frame.height-44, width: 400, height: 44)
-		debuggingLabel.backgroundColor = .black;
-		debuggingLabel.textColor = .white;
-		view.addSubview(debuggingLabel)
+        
+        //Three Finger Tap for Reset
+        let threeFingerTap = UITapGestureRecognizer()
+        threeFingerTap.numberOfTouchesRequired = 3
+        threeFingerTap.addTarget(self, action: #selector(didThreeFingerTap))
+        sceneView.addGestureRecognizer(threeFingerTap)
+        
+        
+        //Debugging Label
+        debuggingLabel.frame = CGRect(x: 0, y: self.view.frame.height-44, width: 400, height: 44)
+        debuggingLabel.backgroundColor = .black;
+        debuggingLabel.textColor = .white;
+        view.addSubview(debuggingLabel)
     }
     
     // this func from Apple ARKit placing objects demo
@@ -52,13 +50,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         sceneView.scene.lightingEnvironment.intensity = intensity
     }
-	
-	func getConfiguration() -> ARWorldTrackingConfiguration {
-		let configuration = ARWorldTrackingConfiguration()
-		configuration.planeDetection = .horizontal
-		configuration.isLightEstimationEnabled = true
-		return configuration
-	}
+    
+    func getConfiguration() -> ARWorldTrackingConfiguration {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        configuration.isLightEstimationEnabled = true
+        return configuration
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -71,146 +69,137 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    private func anyPlaneFrom(location:CGPoint) -> (SCNNode, SCNVector3)? {
-        let results = sceneView.hitTest(location,
-                                        types: ARHitTestResult.ResultType.existingPlaneUsingExtent)
-        
-        print("anyPlaneFrom results \(results)")
-        guard results.count > 0,
-            let anchor = results[0].anchor,
-            let node = sceneView.node(for: anchor) else { return nil }
-        
-        return (node, SCNVector3.positionFromTransform(results[0].worldTransform))
-    }
-    
     var fullWalls = [SCNNode]()
     var doorWalls = [SCNNode]()
     
     @objc func didTap(_ sender:UITapGestureRecognizer) {
-
-		debuggingLabel.text = "Tap, tapped."
+        
+        debuggingLabel.text = "Tap, tapped."
         let location = sender.location(in: sceneView)
         
         print("didTap \(location)")
         
-        guard currentPlane == nil,
-            let newPlaneData = anyPlaneFrom(location: location) else { return }
+        let hitTestResults = sceneView.hitTest(location, types: [.featurePoint, .existingPlaneUsingExtent])
         
-        
-        print("adding wall???")
-        currentPlane = newPlaneData.0
-		
-		
-		fullWalls.forEach{
-            $0.removeFromParentNode()
-        }
-        doorWalls.forEach{
-            $0.removeFromParentNode()
-        }
-        fullWalls = [SCNNode]()
-        doorWalls = [SCNNode]()
-
-        let wallNode = SCNNode()
-		let positionAddition = SCNVector3(newPlaneData.1.x, newPlaneData.1.y, newPlaneData.1.z-2.0)
-		wallNode.position = positionAddition
-		//wallNode.rotation = SCNVector4(x: 1, y: 0, z: 0, w: Float(M_PI/2.0))
-        
-        let sideLength = Nodes.WALL_LENGTH * 3
-        let halfSideLength = sideLength * 0.5
-        
-        let endWallSegmentNode = Nodes.wallSegmentNode(length: sideLength,
-                                                       maskXUpperSide: true)
-        endWallSegmentNode.eulerAngles = SCNVector3(0, 90.0.degreesToRadians, 0)
-        endWallSegmentNode.position = SCNVector3(0, Float(Nodes.WALL_HEIGHT * 0.5), Float(Nodes.WALL_LENGTH) * -1.5)
-        
-        wallNode.addChildNode(endWallSegmentNode)
-        fullWalls.append(endWallSegmentNode)
-        
-        let sideAWallSegmentNode = Nodes.wallSegmentNode(length: sideLength,
+        if let closestResult = hitTestResults.first {
+            print("2----------------")
+            // Get Coordinates of HitTest
+            let transform : matrix_float4x4 = closestResult.worldTransform
+            //sceneView.session.add(anchor: ARAnchor(transform: transform))
+            let worldCoord = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+            
+            print("adding wall???")
+            
+            fullWalls.forEach{
+                $0.removeFromParentNode()
+            }
+            doorWalls.forEach{
+                $0.removeFromParentNode()
+            }
+            fullWalls = [SCNNode]()
+            doorWalls = [SCNNode]()
+            
+            let wallNode = SCNNode()
+            wallNode.position = worldCoord
+            //wallNode.rotation = SCNVector4(x: 1, y: 0, z: 0, w: Float(M_PI/2.0))
+            
+            let sideLength = Nodes.WALL_LENGTH * 3
+            let halfSideLength = sideLength * 0.5
+            
+            let endWallSegmentNode = Nodes.wallSegmentNode(length: sideLength,
+                                                           maskXUpperSide: true)
+            endWallSegmentNode.eulerAngles = SCNVector3(0, 90.0.degreesToRadians, 0)
+            endWallSegmentNode.position = SCNVector3(0, Float(Nodes.WALL_HEIGHT * 0.5), Float(Nodes.WALL_LENGTH) * -1.5)
+            
+            wallNode.addChildNode(endWallSegmentNode)
+            fullWalls.append(endWallSegmentNode)
+            
+            let sideAWallSegmentNode = Nodes.wallSegmentNode(length: sideLength,
+                                                             maskXUpperSide: true)
+            sideAWallSegmentNode.eulerAngles = SCNVector3(0, 180.0.degreesToRadians, 0)
+            sideAWallSegmentNode.position = SCNVector3(Float(Nodes.WALL_LENGTH) * -1.5, Float(Nodes.WALL_HEIGHT * 0.5), 0)
+            wallNode.addChildNode(sideAWallSegmentNode)
+            fullWalls.append(sideAWallSegmentNode)
+            
+            let sideBWallSegmentNode = Nodes.wallSegmentNode(length: sideLength,
+                                                             maskXUpperSide: true)
+            sideBWallSegmentNode.position = SCNVector3(Float(Nodes.WALL_LENGTH) * 1.5, Float(Nodes.WALL_HEIGHT * 0.5), 0)
+            wallNode.addChildNode(sideBWallSegmentNode)
+            fullWalls.append(sideBWallSegmentNode)
+            
+            let doorSideLength = (sideLength - Nodes.DOOR_WIDTH) * 0.5
+            
+            let leftDoorSideNode = Nodes.wallSegmentNode(length: doorSideLength,
                                                          maskXUpperSide: true)
-        sideAWallSegmentNode.eulerAngles = SCNVector3(0, 180.0.degreesToRadians, 0)
-        sideAWallSegmentNode.position = SCNVector3(Float(Nodes.WALL_LENGTH) * -1.5, Float(Nodes.WALL_HEIGHT * 0.5), 0)
-        wallNode.addChildNode(sideAWallSegmentNode)
-        fullWalls.append(sideAWallSegmentNode)
-        
-        let sideBWallSegmentNode = Nodes.wallSegmentNode(length: sideLength,
-                                                         maskXUpperSide: true)
-        sideBWallSegmentNode.position = SCNVector3(Float(Nodes.WALL_LENGTH) * 1.5, Float(Nodes.WALL_HEIGHT * 0.5), 0)
-        wallNode.addChildNode(sideBWallSegmentNode)
-        fullWalls.append(sideBWallSegmentNode)
-        
-        let doorSideLength = (sideLength - Nodes.DOOR_WIDTH) * 0.5
-        
-        let leftDoorSideNode = Nodes.wallSegmentNode(length: doorSideLength,
-                                                     maskXUpperSide: true)
-        leftDoorSideNode.eulerAngles = SCNVector3(0, 270.0.degreesToRadians, 0)
-        leftDoorSideNode.position = SCNVector3(Float(-halfSideLength + 0.5 * doorSideLength),
-                                               Float(Nodes.WALL_HEIGHT) * Float(0.5),
-                                               Float(Nodes.WALL_LENGTH) * 1.5)
-        wallNode.addChildNode(leftDoorSideNode)
-        doorWalls.append(leftDoorSideNode)
-        
-        let rightDoorSideNode = Nodes.wallSegmentNode(length: doorSideLength,
-                                                      maskXUpperSide: true)
-        rightDoorSideNode.eulerAngles = SCNVector3(0, 270.0.degreesToRadians, 0)
-        rightDoorSideNode.position = SCNVector3(Float(halfSideLength - 0.5 * doorSideLength),
-                                                Float(Nodes.WALL_HEIGHT) * Float(0.5),
+            leftDoorSideNode.eulerAngles = SCNVector3(0, 270.0.degreesToRadians, 0)
+            leftDoorSideNode.position = SCNVector3(Float(-halfSideLength + 0.5 * doorSideLength),
+                                                   Float(Nodes.WALL_HEIGHT) * Float(0.5),
+                                                   Float(Nodes.WALL_LENGTH) * 1.5)
+            wallNode.addChildNode(leftDoorSideNode)
+            doorWalls.append(leftDoorSideNode)
+            
+            let rightDoorSideNode = Nodes.wallSegmentNode(length: doorSideLength,
+                                                          maskXUpperSide: true)
+            rightDoorSideNode.eulerAngles = SCNVector3(0, 270.0.degreesToRadians, 0)
+            rightDoorSideNode.position = SCNVector3(Float(halfSideLength - 0.5 * doorSideLength),
+                                                    Float(Nodes.WALL_HEIGHT) * Float(0.5),
+                                                    Float(Nodes.WALL_LENGTH) * 1.5)
+            wallNode.addChildNode(rightDoorSideNode)
+            doorWalls.append(rightDoorSideNode)
+            
+            let aboveDoorNode = Nodes.wallSegmentNode(length: Nodes.DOOR_WIDTH,
+                                                      height: Nodes.WALL_HEIGHT - Nodes.DOOR_HEIGHT)
+            aboveDoorNode.eulerAngles = SCNVector3(0, 270.0.degreesToRadians, 0)
+            aboveDoorNode.position = SCNVector3(0,
+                                                Float(Nodes.WALL_HEIGHT) - Float(Nodes.WALL_HEIGHT - Nodes.DOOR_HEIGHT) * 0.5,
                                                 Float(Nodes.WALL_LENGTH) * 1.5)
-        wallNode.addChildNode(rightDoorSideNode)
-        doorWalls.append(rightDoorSideNode)
-        
-        let aboveDoorNode = Nodes.wallSegmentNode(length: Nodes.DOOR_WIDTH,
-                                                  height: Nodes.WALL_HEIGHT - Nodes.DOOR_HEIGHT)
-        aboveDoorNode.eulerAngles = SCNVector3(0, 270.0.degreesToRadians, 0)
-        aboveDoorNode.position = SCNVector3(0,
-                                            Float(Nodes.WALL_HEIGHT) - Float(Nodes.WALL_HEIGHT - Nodes.DOOR_HEIGHT) * 0.5,
-                                            Float(Nodes.WALL_LENGTH) * 1.5)
-        wallNode.addChildNode(aboveDoorNode)
-        
-        let floorNode = Nodes.plane(pieces: 3,
-                                    maskYUpperSide: false)
-        floorNode.position = SCNVector3(0, 0, 0)
-        wallNode.addChildNode(floorNode)
-        
-        let roofNode = Nodes.plane(pieces: 3,
-                                   maskYUpperSide: true)
-        roofNode.position = SCNVector3(0, Float(Nodes.WALL_HEIGHT), 0)
-        wallNode.addChildNode(roofNode)
-        
-        sceneView.scene.rootNode.addChildNode(wallNode)
-        
-        
-        // we would like shadows from inside the portal room to shine onto the floor of the camera image(!)
-        let floor = SCNFloor()
-        floor.reflectivity = 0
-        floor.firstMaterial?.diffuse.contents = UIColor.white
-        floor.firstMaterial?.colorBufferWriteMask = SCNColorMask(rawValue: 0)
-        let floorShadowNode = SCNNode(geometry:floor)
-        floorShadowNode.position = newPlaneData.1
-        sceneView.scene.rootNode.addChildNode(floorShadowNode)
-        
-        
-        let light = SCNLight()
-        // [SceneKit] Error: shadows are only supported by spot lights and directional lights
-        light.type = .ambient
-        light.spotInnerAngle = 70
-        light.spotOuterAngle = 120
-        light.zNear = 0.00001
-        light.zFar = 5
-        light.castsShadow = true
-        light.shadowRadius = 200
-        light.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
-        light.shadowMode = .deferred
-        let constraint = SCNLookAtConstraint(target: floorShadowNode)
-        constraint.isGimbalLockEnabled = true
-        let lightNode = SCNNode()
-        lightNode.light = light
-        lightNode.position = SCNVector3(newPlaneData.1.x,
-                                        newPlaneData.1.y + Float(Nodes.DOOR_HEIGHT),
-                                        newPlaneData.1.z - Float(Nodes.WALL_LENGTH))
-        lightNode.constraints = [constraint]
-        sceneView.scene.rootNode.addChildNode(lightNode)
-        addImagesToRoom(images: [(#imageLiteral(resourceName: "cat0"), "Cat 1"),(#imageLiteral(resourceName: "cat1"), "Cat 2"),(#imageLiteral(resourceName: "cat2"), "Cat 3"),(#imageLiteral(resourceName: "cat3"), "Cat 4"),(#imageLiteral(resourceName: "cat4"), "Cat 5"),(#imageLiteral(resourceName: "cat5"), "Cat 6"),(#imageLiteral(resourceName: "cat6"), "Cat 7"),(#imageLiteral(resourceName: "cat7"), "Cat 8"),(#imageLiteral(resourceName: "cat8"), "Cat 9")])
+            wallNode.addChildNode(aboveDoorNode)
+            
+            let floorNode = Nodes.plane(pieces: 3,
+                                        maskYUpperSide: false)
+            floorNode.position = SCNVector3(0, 0, 0)
+            wallNode.addChildNode(floorNode)
+            
+            let roofNode = Nodes.plane(pieces: 3,
+                                       maskYUpperSide: true)
+            roofNode.position = SCNVector3(0, Float(Nodes.WALL_HEIGHT), 0)
+            wallNode.addChildNode(roofNode)
+            
+            sceneView.scene.rootNode.addChildNode(wallNode)
+            
+            
+            // we would like shadows from inside the portal room to shine onto the floor of the camera image(!)
+            let floor = SCNFloor()
+            floor.reflectivity = 0
+            floor.firstMaterial?.diffuse.contents = UIColor.white
+            floor.firstMaterial?.colorBufferWriteMask = SCNColorMask(rawValue: 0)
+            let floorShadowNode = SCNNode(geometry:floor)
+            floorShadowNode.position = worldCoord
+            sceneView.scene.rootNode.addChildNode(floorShadowNode)
+            
+            
+            let light = SCNLight()
+            // [SceneKit] Error: shadows are only supported by spot lights and directional lights
+            light.type = .ambient
+            light.spotInnerAngle = 70
+            light.spotOuterAngle = 120
+            light.zNear = 0.00001
+            light.zFar = 5
+            light.castsShadow = true
+            light.shadowRadius = 200
+            light.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
+            light.shadowMode = .deferred
+            let constraint = SCNLookAtConstraint(target: floorShadowNode)
+            constraint.isGimbalLockEnabled = true
+            let lightNode = SCNNode()
+            lightNode.light = light
+            lightNode.position = SCNVector3(worldCoord.x,
+                                            worldCoord.y + Float(Nodes.DOOR_HEIGHT),
+                                            worldCoord.z - Float(Nodes.WALL_LENGTH))
+            lightNode.constraints = [constraint]
+            sceneView.scene.rootNode.addChildNode(lightNode)
+            addImagesToRoom(images: [(#imageLiteral(resourceName: "cat0"), "Cat 1"),(#imageLiteral(resourceName: "cat1"), "Cat 2"),(#imageLiteral(resourceName: "cat2"), "Cat 3"),(#imageLiteral(resourceName: "cat3"), "Cat 4"),(#imageLiteral(resourceName: "cat4"), "Cat 5"),(#imageLiteral(resourceName: "cat5"), "Cat 6"),(#imageLiteral(resourceName: "cat6"), "Cat 7"),(#imageLiteral(resourceName: "cat7"), "Cat 8"),(#imageLiteral(resourceName: "cat8"), "Cat 9")])
+        }
     }
     
     var imageNodes = [SCNNode]()
@@ -259,7 +248,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 let imageHeight = imageWidth * imageHtoWRatio
                 print("Image, Height: \(imageHeight), Width: \(imageWidth)")
                 let imageNodeGeometry = SCNBox(width: Nodes.WALL_WIDTH, height: imageHeight, length: imageWidth, chamferRadius: 0.1)
-
+                
                 let frameMaterial = SCNMaterial()
                 frameMaterial.diffuse.contents = frameColor
                 let imageMaterial = SCNMaterial()
@@ -311,12 +300,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 let textNode = SCNNode(geometry: textGeometry)
                 textNode.pivot = SCNMatrix4MakeTranslation((maxBound.x - minBound.x)/2,(maxBound.y - minBound.y)/2,0)
                 var textPosition = SCNVector3Make(0, 0, 0)
-                if let imageGeometry = imageNode.geometry as? SCNBox{
+                if let _ = imageNode.geometry as? SCNBox{
                     textPosition.y -= 0.3
                 }
                 textNode.position = textPosition
                 textNode.eulerAngles = SCNVector3(0, 270.0.degreesToRadians, 0)
-    //            textNode.scale = SCNVector3Make(0.5, 0.5, 0.5)
+                //            textNode.scale = SCNVector3Make(0.5, 0.5, 0.5)
                 imageNode.addChildNode(textNode)
             }
         }
@@ -340,31 +329,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
-    // did at plane(?)
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        planeCount += 1
-    }
     
     // did update plane?
     func renderer(_ renderer: SCNSceneRenderer, willUpdate node: SCNNode, for anchor: ARAnchor) {
         
     }
-    
-    // did remove plane?
-    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        if node == currentPlane {
-            //TODO: cleanup
-        }
-        
-        if planeCount > 0 {
-            planeCount -= 1
-        }
-    }
-	
+
     @objc func didThreeFingerTap(_ sender:UITapGestureRecognizer) {
-		debuggingLabel.text = "World Reset"
-		sceneView.session.run(getConfiguration(), options: [.resetTracking, .removeExistingAnchors])
-	}
+        debuggingLabel.text = "World Reset"
+        sceneView.session.run(getConfiguration(), options: [.resetTracking, .removeExistingAnchors])
+    }
 }
 
 
